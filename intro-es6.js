78c66cfa3c1109828132bc85736aa0d06f1361cc
Matthew San Pedro <mattsanpedro76@gmail.com>
2018-02-24 19:09:22 -0800

    add object.assign()

---
+++ intro-es6.js
@@ -0,0 +1,29 @@
+const fakeTitles = [
+  'Pirate Of Reality',
+  'Guardians Of Hell',
+  'Witches With Vigor',
+  'Spies And Heroes',
+  'Robots And Kings',
+];
+
+const abbreviations = fakeTitles.map(
+  title => title.toLowerCase().slice(0, 4));
+
+// equivalent using non-arrow functions
+const abbreviationsEs5 = fakeTitles.map(function(title) { return title.toLowerCase().slice(0, 3); });
+
+console.log(abbreviations); // ['pir', 'gua', 'wit', 'spi', 'rob']
+
+// object.assign
+const objA = {
+  foo: 'foo',
+  bar: 'bar'
+ };
+ 
+ const objB = {
+  foo: 'something else',
+  bizz: 'bizz',
+  bang: 'bang'
+ };
+ 
+ console.log(Object.assign({}, objA, objB)); // => {foo: "something else", bar: "bar", bizz: "bizz", bang: "bang"}
